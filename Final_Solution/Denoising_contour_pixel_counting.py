db1b4f677982752b84c5d7502ad5ddb0cb4735eb
Zeinab Taghavi <zeinabtaghavi1377@gmail.com>
2020-01-29 13:28:07 +0330

    better algorithm

--- Final_Solution/Denoising_contour_pixel_counting.py
+++ Final_Solution/Denoising_contour_pixel_counting.py
@@ -7,21 +7,24 @@ from PIL import Image
 
 
 def correct_rotation(img,max_word_height):# max_word_height = 140
-    
+
     env_img = cv2.bitwise_not(img)
+    env_img = cv2.dilate(env_img, kernel=np.ones((5, 5), np.uint8), iterations=1)
     contours, _ = cv2.findContours(env_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    
+
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)  # position of contour
         if h < max_word_height* 1.5:
             continue
         env_img[y:y + h, x:x + w] = 0
     
-    img = cv2.bitwise_not(env_img)
-    edges = cv2.Canny(img, 50, 150, apertureSize=3)
+    img_temp = cv2.bitwise_not(env_img)
+    edges = cv2.Canny(img_temp, 50, 150, apertureSize=3)
     lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
     num = 0
     sum = 0
+
+
 
     # if there is no line
     try:
@@ -53,6 +56,7 @@ def move_images(img, img_source , borders , image_shape): # borders = [top, righ
     
     img_source_gray = cv2.cvtColor(img_source, cv2.COLOR_BGR2GRAY)
     output_base_image = cv2.bitwise_not(img_source_gray)
+    output_base_image = cv2.dilate(output_base_image ,kernel=np.ones((5,5), np.uint8), iterations=1)
     contours, _ = cv2.findContours(output_base_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     # the image or words must be between 10% and 90% of main image
@@ -63,34 +67,38 @@ def move_images(img, img_source , borders , image_shape): # borders = [top, righ
     min_w = image_shape[0]
     min_h = image_shape[1]
     
-    
+    img_images = np.zeros((img.shape[0],img.shape[1]),np.uint8)
 
     # for any contour in main image , check if it is big enough to ba an image or word
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)  # position of contour
-        if y < max_y and y > min_y and x < max_x and x > min_x and w > min_w and h > min_h:
-            img[y:y + h, x:x + w] = img_source_gray[y:y + h, x:x + w]
 
-    cv2.imwrite(file_name +'-5-move_images.jpg', img)
+        if min_y < y and y+h < max_y  and min_x < x and x+w < max_x and w > min_w and h > min_h:
+            print (x, y, w, h, min_y < y < max_y, min_x < x < max_x, w > min_w, h > min_h)
+            img[y:y + h, x:x + w] = img_source_gray[y:y + h, x:x + w]
+            img_images[y:y + h, x:x + w] = img_source_gray[y:y + h, x:x + w]
+    print (img.shape , max_y ,max_x , min_y, min_x , min_w ,min_h  )
+    cv2.imwrite(file_name +'-5-moved_just_images.jpg', img_images)
     return img
 
 
 
 def remove_wasted_round_area(img, file_name, borders ,first_kernel_erod=(3,3), first_kernel_dilate=(15, 15),pixels_per_slice=20,
-                             block_thresh=10, border_pixel=5, min_contour_area_minimizes_img=2,
+                             block_thresh=10, min_contour_area_minimizes_img=2,
                              last_kernel_dilate=(10, 10)): # borders = [top, right, down, left]
-
+    print(img.shape)
     img[img.shape[0] - borders[2]:,:]=255
     img[:,img.shape[1] - borders[1]:]=255
     img[:borders[0],:]=255
     img[:,0:borders[3]] = 255
 
+    cv2.imwrite(file_name + '-2-0-1-find_wasted_round_area_in_documents-borders.jpg', img)
     gray_env = cv2.bitwise_not(img)
     gray_env_erod = cv2.erode(gray_env, kernel=np.ones(first_kernel_erod, np.uint8), iterations=1)
     # cv2.imwrite(file_name + 'area_erode.jpg', gray_env_erod)
     gray_env_dilate = cv2.dilate(gray_env_erod, kernel=np.ones(first_kernel_dilate, np.uint8), iterations=1)
     # cv2.imwrite(file_name + 'area_dilate.jpg', gray_env_dilate)
-    cv2.imwrite(file_name +'-2-0-find_wasted_round_area_in_documents-gray_env_dilate_.jpg', img)
+    cv2.imwrite(file_name +'-2-0-find_wasted_round_area_in_documents-gray_env_dilate_.jpg', gray_env_dilate)
 
     poly = np.zeros((int(gray_env_dilate.shape[0] / pixels_per_slice), int(gray_env_dilate.shape[1] / pixels_per_slice), 1), np.uint8)
     poly.fill(0)
@@ -104,15 +112,8 @@ def remove_wasted_round_area(img, file_name, borders ,first_kernel_erod=(3,3), f
 
     # cv2.imwrite(file_name +'-2-1-find_wasted_round_area_in_documents-poly_.jpg', poly)
 
-    poly[0:border_pixel, :] = 255
-    poly[poly.shape[0] - border_pixel: poly.shape[0], :] = 255
-    poly[:, 0:border_pixel] = 255
-    poly[:, poly.shape[1] - border_pixel:poly.shape[1]] = 255
-    h, w = poly.shape[:2]
-    mask = np.zeros((h + 2, w + 2), np.uint8)
-    cv2.floodFill(poly, mask, (0, 0), 0)
-
     # cv2.imwrite(file_name +'-2-2-find_wasted_round_area_in_documents-poly_.jpg', poly)
+
 
     contours, _ = cv2.findContours(poly, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -121,9 +122,10 @@ def remove_wasted_round_area(img, file_name, borders ,first_kernel_erod=(3,3), f
             x, y, w, h = cv2.boundingRect(cnt)
             cv2.rectangle(poly, (x, y), (x + w, y + h), 0, -1)
 
+    poly = cv2.dilate(poly, kernel=np.ones(last_kernel_dilate, np.uint8), iterations=1)
     cv2.imwrite(file_name +'-2-3-find_wasted_round_area_in_documents-poly-contoured_.jpg', poly)
 
-    poly = cv2.dilate(poly, kernel=np.ones(last_kernel_dilate, np.uint8), iterations=1)
+
     poly3 = np.zeros((int(gray_env_dilate.shape[0]), int(gray_env_dilate.shape[1]), 1), np.uint8)
     poly3.fill(0)
     for y in range(0, pices[0]):
@@ -139,6 +141,8 @@ def remove_wasted_round_area(img, file_name, borders ,first_kernel_erod=(3,3), f
 
     return no_waisted_area_on_source,no_waisted_area
 
+
+
 def denoise_by_contours(img, img_source, file_name,blur_size=(3, 3), denoise_erode_kernel=(2, 2),
                         borders=[100,100,100,100], contour_min_area=30, min_noise_h=150, max_noise_w = 30):
 
@@ -148,10 +152,14 @@ def denoise_by_contours(img, img_source, file_name,blur_size=(3, 3), denoise_ero
         gray_img = img
 
 
-    blur = cv2.blur(gray_img, blur_size)
-    _, thresh = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY)
+    if blur_size[0]!= 0 and blur_size[1]!=0:
+        blur = cv2.blur(gray_img, blur_size)
+        _, thresh = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY)
+    else:
+        thresh = gray_img
+
     gray_img = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel=np.ones(denoise_erode_kernel, np.uint8))
-    cv2.imwrite( file_name +'-1-1-first_sort_of_noises.jpg', gray_img)
+    # cv2.imwrite( file_name +'-1-1-first_sort_of_noises.jpg', gray_img)
 
     contour, _ = cv2.findContours(gray_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     new_img = np.zeros((img.shape[0], img.shape[1]), np.uint8)
@@ -172,38 +180,38 @@ def denoise_by_contours(img, img_source, file_name,blur_size=(3, 3), denoise_ero
 
         cv2.fillPoly(new_img, [approx], (0, 0, 0))
 
-    cv2.imwrite(file_name + '-1-2-newimage_as_temp.jpg', new_img)
+    # cv2.imwrite(file_name + '-1-2-newimage_as_temp.jpg', new_img)
     mask = cv2.bitwise_or(new_img, gray_img)
 
     # background is inverse => dilate -> erode
-    cv2.imwrite(file_name + '-1-3-mask.jpg', mask)
+    # cv2.imwrite(file_name + '-1-3-mask.jpg', mask)
 
-    img_env_dilate = cv2.dilate(gray_img, kernel=np.ones(blur_size, np.uint8))
-    cv2.imwrite(file_name +'-1-4-mask_dilate.jpg', img_env_dilate)
+    mask_dilate = cv2.erode(mask, kernel=np.ones(blur_size, np.uint8))
+    # cv2.imwrite(file_name +'-1-4-mask_dilate.jpg', mask_dilate)
 
     img_source_gray = cv2.cvtColor(img_source, cv2.COLOR_BGR2GRAY)
-    img = cv2.bitwise_or(mask, img_source_gray)
+    img = cv2.bitwise_or(mask_dilate, img_source_gray)
 
     return img
 
 
 if __name__ =='__main__':
     time = []
-    max_word_height = 140
-    borders = [489, 560, 477, 630] # borders = [top, right, down, left]
+    max_word_height = 190  # max height of line
+    borders = [0, 250, 350, 200] # borders = [top, right, down, left]
     image_dir = './'
-    for i in range(1,2):
+    for i in range(1,13):
         e1 = cv2.getTickCount()
-        file_name = image_dir+'/'+'image'+str(i)+'.bmp'
+        file_name = image_dir+'/'+'image'+str(i)+'.tif'
         img_source = cv2.imread(file_name)
 
         # for first denoising by contour
 
-        blur_size = (3, 3)
-        denoise_erode_kernel = (3, 3)
-        contour_min_area = 100
-        min_noise_h = 150
-        max_noise_w = 5
+        blur_size = (0, 0)  # less than dot's diameter
+        denoise_erode_kernel = (0, 0)  # same as blur
+        contour_min_area = 12  # less than dot's are
+        min_noise_h = 150  # more than biggest lines height
+        max_noise_w = 3  # less than thinnest acceptable line
 
         denoised_by_contours = denoise_by_contours(img_source,img_source,file_name,
                                                    blur_size=blur_size,
@@ -215,23 +223,23 @@ if __name__ =='__main__':
 
         cv2.imwrite(file_name + '-1-denoise_by_contours.jpg', denoised_by_contours)
 
-        first_kernel_erod = (5, 5)
-        first_kernel_dilate = (15, 15)
+        first_kernel_erod = (3, 3) # less than dot's diameter
+        max_space = 30
+        min_word_height = 30
+        first_kernel_dilate = (int(min_word_height/4), max_space)
         pixels_per_slice = 15
         block_thresh = 20
-        border_pixel = 5
-        min_contour_area_minimizes_img = 3
+        min_contour_area_minimizes_img = int((2*(max_space+min_word_height)) / pixels_per_slice)
         last_kernel_dilate = (10, 10)
 
-        removed_wasted_round_area , pattern = remove_wasted_round_area(denoised_by_contours,file_name,borders,
-                                                                       first_kernel_erod=first_kernel_erod,
-                                                                       first_kernel_dilate=first_kernel_dilate,
-                                                                       pixels_per_slice=pixels_per_slice,
-                                                                       block_thresh=pixels_per_slice,
-                                                                       border_pixel=border_pixel,
-                                                                       min_contour_area_minimizes_img=min_contour_area_minimizes_img,
-                                                                       last_kernel_dilate=last_kernel_dilate
-                                                                       )
+        removed_wasted_round_area, pattern = remove_wasted_round_area(denoised_by_contours, file_name, borders,
+                                                                      first_kernel_erod=first_kernel_erod,
+                                                                      first_kernel_dilate=first_kernel_dilate,
+                                                                      pixels_per_slice=pixels_per_slice,
+                                                                      block_thresh=pixels_per_slice,
+                                                                      min_contour_area_minimizes_img=min_contour_area_minimizes_img,
+                                                                      last_kernel_dilate=last_kernel_dilate
+                                                                      )
 
         cv2.imwrite(file_name + '-2-removed_wasted_round_area_pattern.jpg', pattern)
         cv2.imwrite(file_name + '-2-removed_wasted_round_area_img.jpg', removed_wasted_round_area)
